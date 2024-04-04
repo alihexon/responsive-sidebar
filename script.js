1f3d3fc35c3fbcd0839df45b5fb0688792c09f82
hexoN <aliakbaripour00@gmail.com>
2024-04-04 16:50:31 +0330

    Finished and fixed the script + some css works

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-const closeButton = document.querySelector('.close-btn')
-const closeIcon = document.querySelector('.js-icon')
-const menu = document.querySelector('aside')
+const closeButton = document.querySelector('.close-btn');
+const closeIcon = document.querySelector('.js-icon');
+const menu = document.querySelector('aside');
+const userInfo = document.querySelector('.user-info');
+const navigationTexts = document.querySelectorAll('aside nav ul li a');
 
 function switchMenu() {
-  menu.classList.toggle('closed')
+  menu.classList.toggle('closed');
+  userInfo.classList.toggle('hidden');
+  navigationTexts.forEach(navigationText => {
+    navigationText.classList.toggle('hidden');
+  });
+
   
   if (closeIcon.classList.contains('bx-chevron-left')) {
     closeIcon.classList.replace('bx-chevron-left', 'bx-chevron-right');
